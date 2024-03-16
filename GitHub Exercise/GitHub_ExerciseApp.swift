11f5ae32bf075b7d6d8a49467e663df7f124a592
charles2m <ch.mmartiniere@hotmail.com>
2024-03-16 21:57:23 +0100

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Charles Martin Martinière on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
